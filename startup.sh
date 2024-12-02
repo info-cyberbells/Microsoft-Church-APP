d60fb947973b24aa41f3c5b0e9300895840e5921
Cyberbells <info.cyberbells@gmail.com>
2024-12-02 12:30:42 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 cd /home/site/wwwroot
-python -m gunicorn --bind=0.0.0.0:8000 --workers=4 --timeout=120 --access-logfile='-' --error-logfile='-' --log-level='info' --keep-alive=60 application:app
+gunicorn --bind=0.0.0.0:8000 \
+         --workers=4 \
+         --timeout=120 \
+         --access-logfile=- \
+         --error-logfile=- \
+         --log-level=info \
+         application:app
