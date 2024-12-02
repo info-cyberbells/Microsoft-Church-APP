263c4122a9ea7482fade5fb1c02631bf3369fdf1
Cyberbells <info.cyberbells@gmail.com>
2024-12-02 18:29:30 +0530

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,14 +1,20 @@
 import multiprocessing
+import os
+
+# Get the HOME directory from environment
+home_dir = os.environ.get('HOME', '/tmp')
+log_dir = os.path.join(home_dir, 'LogFiles')
+os.makedirs(log_dir, exist_ok=True)
 
 # Basic Configuration
-bind = "0.0.0.0:8000"  # Azure expects port 8000
+bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = 'sync'
 threads = 2
 
 # Logging Configuration
-accesslog = "/home/LogFiles/gunicorn_access.log"
-errorlog = "/home/LogFiles/gunicorn_error.log"
+accesslog = os.path.join(log_dir, "gunicorn_access.log")
+errorlog = os.path.join(log_dir, "gunicorn_error.log")
 loglevel = 'info'
 capture_output = True
 enable_stdio_inheritance = True
