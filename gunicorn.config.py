4f711250a1ce3377c8eac3627abd218c43e11b11
Cyberbells <info.cyberbells@gmail.com>
2024-12-02 11:47:09 +0530

    Update gunicorn.conf.py

--- gunicorn.config.py
+++ gunicorn.config.py
@@ -2,7 +2,7 @@ import os
 import multiprocessing
 
 # Basic Configuration
-bind = "0.0.0.0:7676"
+bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = 'sync'
 threads = 4
