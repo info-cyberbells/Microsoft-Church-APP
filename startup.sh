2b249c414d124ef3b1bc1a614fa2a1d61e673505
Cyberbells <info.cyberbells@gmail.com>
2024-12-02 18:30:27 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-cd /home/site/wwwroot
-gunicorn --bind=0.0.0.0:8000 \
-         --workers=4 \
-         --timeout=120 \
-         --access-logfile=- \
-         --error-logfile=- \
-         --log-level=info \
-         application:app
+
+# Create necessary directories
+mkdir -p "$HOME/LogFiles"
+chmod 755 "$HOME/LogFiles"
+
+# Start Gunicorn
+exec gunicorn --config=gunicorn.conf.py application:app
