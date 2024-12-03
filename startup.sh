8d24283e12e4d7eeb8fed655c3569fce7bea59fa
Cyberbells <info.cyberbells@gmail.com>
2024-12-03 12:22:00 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Install gevent if not already installed
+pip install gevent
+
 # Create necessary directories
 mkdir -p "$HOME/LogFiles"
 chmod 755 "$HOME/LogFiles"
 
-# Start Gunicorn
+# Start Gunicorn with gevent worker
 exec gunicorn --config=gunicorn.conf.py application:app
