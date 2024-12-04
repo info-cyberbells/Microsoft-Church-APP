5e7d4db32512a3a48e2d9f8e2470f14cbab67f7d
Cyberbells <info.cyberbells@gmail.com>
2024-12-04 11:35:47 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -7,21 +7,5 @@ pip install gevent
 mkdir -p "$HOME/LogFiles"
 chmod 755 "$HOME/LogFiles"
 
-mkdir -p /home/site/libs
-cd /home/site/libs
-
-# Updated package URLs for Ubuntu 20.04
-wget http://archive.ubuntu.com/ubuntu/pool/main/a/alsa-lib/libasound2_1.2.4-1_amd64.deb
-wget http://archive.ubuntu.com/ubuntu/pool/main/p/portaudio19-dev/libportaudio2_19.6.0-1_amd64.deb
-wget http://archive.ubuntu.com/ubuntu/pool/main/p/pulseaudio/libpulse0_13.99.1-1ubuntu3.13_amd64.deb
-
-dpkg -x libasound2_1.2.4-1_amd64.deb /home/site/libs
-dpkg -x libportaudio2_19.6.0-1_amd64.deb /home/site/libs  
-dpkg -x libpulse0_13.99.1-1ubuntu3.13_amd64.deb /home/site/libs
-
-export LD_LIBRARY_PATH=/home/site/libs/usr/lib/x86_64-linux-gnu
-
-python application.py
-
 # Start Gunicorn with gevent worker
 exec gunicorn --config=gunicorn.conf.py application:app
