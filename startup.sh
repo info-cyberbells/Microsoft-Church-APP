34f2ba1d2c41d97c4f37c4eb9769ff680e43f502
Cyberbells <info.cyberbells@gmail.com>
2024-12-11 22:59:58 +0530

    build asses added.

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y python3-pyaudio portaudio19-dev python3-dev
+sudo apt-get install -y python3-pyaudio portaudio19-dev python3-dev build-essential
 
 # Install gevent if not already installed
 pip install gevent
