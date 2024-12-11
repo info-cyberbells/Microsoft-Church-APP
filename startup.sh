307331890e82946d0a74e5631666defd5cc7bd35
Cyberbells <info.cyberbells@gmail.com>
2024-12-11 22:33:29 +0530

    Updated startup.

--- startup.sh
+++ startup.sh
@@ -8,4 +8,4 @@ mkdir -p "$HOME/LogFiles"
 chmod 755 "$HOME/LogFiles"
 
 # Start Gunicorn with gevent worker
-exec gunicorn --config="/gunicorn.conf.py" application:app
+exec gunicorn  --bind="0.0.0.0:8000"  --workers=2 --worker_class="gthread" --threads=2 --accesslog="/home/LogFiles/gunicorn_access.log" --errorlog="/home/LogFiles/gunicorn_error.log" --loglevel="info" --capture_output=True --enable_stdio_inheritance=True --timeout=120 --keepalive=60 --wsgi_app="application:app" --proc_name="church-app" --worker_tmp_dir="/tmp" --max_requests=1000 --max_requests_jitter=50 --reload=False --daemon=False application:app
