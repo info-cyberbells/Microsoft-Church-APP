41f742e2c12d9b2b5ceafa833d7e6fe461fda6a6
Cyberbells <info.cyberbells@gmail.com>
2024-12-16 18:19:10 +0530

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,6 @@
 import multiprocessing
 # Basic Configuration
-bind = "0.0.0.0:8000"  # Azure expects port 8000
+bind = "0.0.0.0:8010"  # Azure expects port 8000
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = 'gthread'
 threads = 2
